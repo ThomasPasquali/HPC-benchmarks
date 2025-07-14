9ab693d79f9acbc0c707605f3b21012402514329
Thomas Pasquali <tom.pasqui00@gmail.com>
2025-07-14 19:42:19 +0200

    Graph500 customized code draft + HAICGU scripts

---
+++ Graph500/compile_haicgu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+ml GCC/14.1.0 OpenMPI/5.0.3
+
+cd graph500/src
+mkdir -p ../../bin
+
+# Standard benchmark
+make clean
+CFLAGS="-DBENCHPIN" make
+mv graph500_reference_bfs_sssp ../../bin
+
+# Benchmarks that flushes the buffer more often: after 512 vertices instead of 8192
+make clean
+CFLAGS="-DBENCHPIN" PREPROCESSOR_FLAGS="-DAGGR_intra=2048 -DAGGR=2048" make
+mv graph500_reference_bfs_sssp ../../bin/graph500_reference_bfs_sssp_smallbuf
