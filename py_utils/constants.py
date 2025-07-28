b13d44f5b64c56dee8fa6bd346e64c51c56220c2
Thomas Pasquali <tom.pasqui00@gmail.com>
2025-07-28 19:20:40 +0200

    More configurations

--- py_utils/constants.py
+++ py_utils/constants.py
@@ -19,12 +19,20 @@ BOARD_NAMES_MAP = {
   'bananaf3': 'Banana Pi F3',
   'arriesgado': 'HiFive Unmatched',
 }
+BOARD_SHORT_NAMES_MAP = {
+  'brah': 'AMD',
+  'baldo': 'AMD',
+  'pioneer': 'Pioneer',
+  'bananaf3': 'BananaPi',
+  'arriesgado': 'HiFive',
+}
 
 # Hard-coded data
 CACHE_SIZES = {
-  'pioneer':    [(64 * 1024, 'L1 cache'), (1 * 1024 * 1024, 'L2 cache'),  (64 * 1024 * 1024, 'L3 cache')],
-  'bananaf3':   [(32 * 1024, 'L1 cache'), (512 * 1024     , 'L2 cache'),  (512 * 1024,       'TCM')     ],
-  'arriesgado': [(64 * 1024, 'L1 cache'), (2 * 1024 * 1024, 'L2 cache'),  (0,                '')        ],
+  'pioneer':    [(64 * 1000,        'L1d'), (1 * 1000 * 1000,  'L2'),  (64 * 1000 * 1000,  'L3')],
+  'bananaf3':   [(32 * 1000,        'L1d'), (500 * 1000     ,  'L2'),  (2* 500 * 1000,     'L2 + TCM')],
+  'arriesgado': [(64 * 1000,        'L1d'), (2 * 1000 * 1000,  'L2'),  (0,                 '')        ],
+  'brah':       [(4 * 1000 * 1000,  'L1d'), (64 * 1000 * 1000, 'L2'),  (512 * 1000 * 1000, 'L3')],
 }
 
 # Plots style
