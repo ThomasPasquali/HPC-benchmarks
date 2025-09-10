7fcad47069206d5b9983d72056835e16122c10e2
Thomas Pasquali <tom.pasqui00@gmail.com>
2025-09-10 17:53:49 +0200

    small fix

--- HPL/gen_dat_file.sh
+++ HPL/gen_dat_file.sh
@@ -45,8 +45,8 @@ NB = $hpl_NB
 val = (totalmem * (1024**3) * nodes / 8.0)**0.5
 N = int(round(val / NB) * NB)
 if nodes >= 4:
-  N = int(0.65*N)
-N = N / 2.0
+  N = int(0.7*N)
+N = N * 0.8
 print(N)
 EOF
 )
