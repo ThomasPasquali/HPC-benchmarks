98e852af2146a1adfdd0d70ebcc1d1fad0de72f4
Jacopo Raffi <jraffi@dev.guoehi.cluster>
2025-12-16 14:02:53 +0000

    Remove BLAS dependency

--- HPCG/build_makefile.sh
+++ HPCG/build_makefile.sh
@@ -8,24 +8,31 @@ BRANCHNAME=master
 
 ARCH=$1
 
-LAlib=$2
-LAname=$3
-LAinc=$4
+#LAlib=$2
+#LAname=$3
+#LAinc=$4
 
-MPlib=$5
-MPname=$6
-MPinc=$7
+MPlib=$2
+MPname=$3
+MPinc=$4
 
 HPCG_PATH=${8:-'hpcg-cpu'}
 TOPdir="$(pwd)/${HPCG_PATH}"
 
-if [[ -z "$ARCH" || -z "$LAlib" || -z "$LAinc"  || -z "$LAname" || -z "$MPlib" || -z "$MPname" || -z "$MPinc" ]]; then
+if [[ -z "$ARCH" || -z "$MPlib" || -z "$MPname" || -z "$MPinc" ]]; then
   echo "Error: Missing required arguments."
   echo "Usage: $0 <ARCH> <LAlib> <LAname> <LAinc> <MPlib> <MPname> <MPinc> [HPCG_PATH]"
   exit 1
 fi
 
-sed "s|#ARCH#|${ARCH}|g; s|#LAlib#|${LAlib}|g; s|#LAname#|${LAname}|g; s|#LAinc#|${LAinc}|g; s|#MPlib#|${MPlib}|g; s|#MPname#|${MPname}|g; s|#MPinc#|${MPinc}|g; s|#TOPdir#|${TOPdir}|g" Make.in > Make.out
+
+#if [[ -z "$ARCH" || -z "$LAlib" || -z "$LAinc"  || -z "$LAname" || -z "$MPlib" || -z "$MPname" || -z "$MPinc" ]]; then
+#  echo "Error: Missing required arguments."
+#  echo "Usage: $0 <ARCH> <LAlib> <LAname> <LAinc> <MPlib> <MPname> <MPinc> [HPCG_PATH]"
+#  exit 1
+#fi
+
+sed "s|#ARCH#|${ARCH}|g; s|#MPlib#|${MPlib}|g; s|#MPname#|${MPname}|g; s|#MPinc#|${MPinc}|g; s|#TOPdir#|${TOPdir}|g" Make.in > Make.out
 
 # if [[ ! -d ${HPCG_PATH} ]]; then
 #   git clone -b $BRANCHNAME --recursive https://github.com/hpcg-benchmark/hpcg.git ${HPCG_PATH}
